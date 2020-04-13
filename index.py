b409ea9b338ad967e046be66363a61d0907856e3
sachinm78 <sachinm78@gmail.com>
2020-04-13 12:07:49 -0500

    set up loop with user inputs and results

--- index.py
+++ index.py
@@ -9,12 +9,15 @@ no_questions = int(input('How many questions do you want?: '))
 score = 0
 
 # loop through number of questions
-
 # create two random numbers and calc answer
-
 # show user the question
-
 # capture answer and modify user score
-
 # output final score 
+for q in range(no_questions):
+    num1,num2 = r(1,11),r(1,11)
+    ans = num1 * num2
+    u_ans =int(input(f'{num1} X {num2} = '))
+    if u_ans == ans:
+        score += 1
+print(f'Thank you for playing! \nYou got {score} out of {no_questions} {round(score/no_questions*100)}%)')
 
