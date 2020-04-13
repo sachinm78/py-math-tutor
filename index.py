068fecc8d61d6d3e13f2c5b2c4254ccaa57b8f3c
sachinm78 <sachinm78@gmail.com>
2020-04-13 12:21:12 -0500

    added calculation for seconds per question

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 print('Project - Math Tutor')
 #import modules
 from  random import randrange as r 
+from time import time as t
 
 # ask how many questions user wants
 no_questions = int(input('How many questions do you want?: '))
@@ -13,11 +14,12 @@ score = 0
 # show user the question
 # capture answer and modify user score
 # output final score 
+start = t()
 for q in range(no_questions):
     num1,num2 = r(1,11),r(1,11)
     ans = num1 * num2
     u_ans =int(input(f'{num1} X {num2} = '))
     if u_ans == ans:
         score += 1
-print(f'Thank you for playing! \nYou got {score} out of {no_questions} {round(score/no_questions*100)}%)')
-
+    end = t()
+print(f'Thank you for playing! \nYou got {score} out of {no_questions} ({round(score/no_questions*100)}%) correct in {round(end-start,1)} seconds ({round((end-start)/no_questions,1)}seconds/question)')
