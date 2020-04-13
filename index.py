e49a97c7dd08b190f8dfd73f16dd9b546c8d2ac6
sachinm78 <sachinm78@gmail.com>
2020-04-13 11:53:15 -0500

    wrote out pseudo code for project

---
+++ index.py
@@ -0,0 +1,10 @@
+print('Project - Math Tutor')
+# import modules
+# ask how many questions user wants
+# set score start at zero
+# loop through number of questions
+# create two random numbers and calc answer
+# show user the question
+# capture answer and modify user score
+# output final score 
+
