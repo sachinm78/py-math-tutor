507d9a59f8c227cd190c29a38a6e297c12802e33
sachinm78 <sachinm78@gmail.com>
2020-04-13 11:57:00 -0500

    imported random and set up user input

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 print('Project - Math Tutor')
-# import modules
+#import modules
+from  random import randrange as r 
+
 # ask how many questions user wants
-# set score start at zero
+no_questions = int(input('How many questions do you want?: '))
+
+#set score start at zero
+score = 0
+
 # loop through number of questions
+
 # create two random numbers and calc answer
+
 # show user the question
+
 # capture answer and modify user score
+
 # output final score 
 
